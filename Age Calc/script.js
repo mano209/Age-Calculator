958101f6ea97e2d3adbce69d66bb3fcada2d9e31
Mano B <141111707+mano209@users.noreply.github.com>
2024-06-02 23:22:59 +0530

    Add files via upload

---
+++ Age Calc/script.js
@@ -0,0 +1,21 @@
+// let currDate = document.getElementById('currDate');
+// let dateOfBirth = document.querySelector('#DOB');
+// const calcAge = document.getElementById('CalcAge');
+// const displayAge = document.getElementById('displayAge');
+// const Age = document.getElementById('age');
+
+// var today = new Date();
+// currDate.innerText=`Today's date is : ${today.toLocaleDateString('en-US')}`;
+
+// calcAge.addEventListener('click',() => {
+//     var birthDate = new Date(dateOfBirth.value);
+//     var age = today.getFullYear() - birthDate.getFullYear();
+//     var m = today.getMonth() - birthDate.getMonth();
+
+//     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//         age = age - 1;
+//     }
+//     displayAge.style.visibility = "visible";
+//     Age.innerText = `You are ${age} years old.`
+// });
+
